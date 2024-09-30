d0c9f13678ffa9a2f91f73ac8f0c1ccc092a1afa
Quentin-Epitech <quentin.bonnet@epitech.digital>
2024-09-30 11:35:25 +0200

    def avec typeof

---
+++ ex_04.js
@@ -0,0 +1,7 @@
+document.write(typeof undef + '<br>');
+document.write(typeof true + '<br>');
+document.write(typeof 42 + '<br>');
+document.write(typeof 1211.56789 + '<br>');
+document.write(typeof Symbol() + '<br>');
+document.write(typeof [] + '<br>');
+document.write(typeof a() + '<br>');
