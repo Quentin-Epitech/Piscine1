959d80012e4e3315121075c86efdacc949691a97
Quentin-Epitech <quentin.bonnet@epitech.digital>
2024-09-30 16:20:45 +0200

    input

--- ex_09.js
+++ ex_09.js
@@ -8,17 +8,22 @@ else if (typeof input_var === "boolean") {
 else if (typeof input_var === "number") {
 	if (Number.isInteger(input_var)){
   	type = "integer";
-    
-  }else {
-  		type = "Float";
+}
+else if (typeof Number.isInteger(input_var))
+{type = "Float";
+}
+else {
+  		type = "inconnu";
   }
 }
 
 displayThisText("The variable value is" +input_var);
 displayThisText("Its type is" + type);
-	if (input_var === 42) {
-  displayThisText("It is the meaning of life.")
-  }
+
+if (input_var === 42) 
+{displayThisText("It is the meaning of life.") }
+
+  
 
 
 
