dd630aba7e4259e34d8cba07f17e74b8b128fbcd
Quentin-Epitech <quentin.bonnet@epitech.digital>
2024-09-30 10:43:43 +0200

    exo2

---
+++ ex_02.js
@@ -0,0 +1,10 @@
+let string = "Forty Two";
+let integer = 42;
+let float= 42.42;
+let this_is_null= "" ;
+ 
+displayThisText(bool)
+displayThisText(string)
+displayThisText(integer)
+displayThisText(float)
+displayThisText(this_is_null);
