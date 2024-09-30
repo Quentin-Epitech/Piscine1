286e9553f6f602a11723849213e37f38452983c6
Quentin-Epitech <quentin.bonnet@epitech.digital>
2024-09-30 14:48:07 +0200

    input_var variable

---
+++ ex_09.js
@@ -0,0 +1,24 @@
+let type;
+if (typeof input_var === "string") {
+		type = 'string'
+} 
+else if (typeof input_var === "boolean") {
+	type = "boolean";
+}
+else if (typeof input_var === "number") {
+	if (Number.isInteger(input_var)){
+  	type = "integer";
+    
+  }else {
+  		type = "Float";
+  }
+}
+
+displayThisText(`The variable value is ${input_var}`);
+displayThisText(`Its type is ${type}`);
+	if (input_var === 42) {
+  displayThisText("It is the meaning of life.")
+  }
+
+
+
